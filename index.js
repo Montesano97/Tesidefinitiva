ead59706bfb63f23de950e64a1cdbc5166cc37a3
Montesano97 <155659334+Montesano97@users.noreply.github.com>
2024-02-26 20:27:18 +0100

    invio

--- index.js
+++ index.js
@@ -15,31 +15,23 @@ const User = require("./model/User");
 const Mod = require("./model/Mod");
 
 
+
 const path = require("path");
 app.set("view engine", "html");
 app.engine("html", require("ejs").renderFile);
 
 //connessione al db Ospedaledb su cluster Mongodb
 
-const dbUrl = process.env.MONGODB_URI || "mongodb+srv://Gianni:1234@clustervincenzo.3glcyhq.mongodb.net/OspedaleDb?retryWrites=true&w=majority";
 
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000,
-})
-.then(() => console.log("Database registrazione connesso"))
-.catch(err => console.error("Errore di connessione al database:", err));
-
-
-
-
+// Connessione a MongoDB Atlas con Mongoose
+const dbUrl = process.env.MONGODB_URI || 'mongodb+srv://Gianni:1234@clustervincenzo.3glcyhq.mongodb.net/OspedaleDb?retryWrites=true&w=majority';
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
     //creazione modello user=paziente
     //const User = mongoose.model("User", userSchema);
 
     //dati registrazione profilo paziente datab
-    
+
     const notesSchema = {
     nome: String,
     cognome: String,
@@ -48,7 +40,7 @@ mongoose.connect(dbUrl, {
     residenza: String,
     telefono: Number,
     email: String
-    
+
 };
 
 
@@ -66,30 +58,30 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
     try {
-        res.sendFile(__dirname + "/pub/index.html");
+        res.sendFile(__dirname + "index.html");
         if (!req.body.nome || !req.body.cognome) {
             return res.status(400).send("Dati non compilati in modo corretto.");
         }
 // recupera dati da form in index.html
-        let newNote = new Note({
-            nome: req.body.nome,
-            cognome: req.body.cognome,
-            nato: req.body.nato,
-            data: req.body.data,
-            residenza: req.body.residenza,
-            telefono: req.body.telefono,
-            email: req.body.email
-            
-        });
+      let newNote = new Note({
+        nome: req.body.nome,
+        cognome: req.body.cognome,
+        nato: req.body.nato,
+        data: req.body.data,
+        residenza: req.body.residenza,
+        telefono: req.body.telefono,
+        email: req.body.email
+      });
 
-        await newNote.save();
+      await newNote.save();
 
-        // Genera e salva il PDF 
-        generatePDF(newNote);
+      // Genera e salva il PDF 
+      generatePDF(newNote);
 
-        res.redirect("/");
-    } catch (error) {
-        console.error("Errore durante la gestione della richiesta POST:", error);
+      console.log("Dati salvati con successo nel database:", newNote);
+      res.redirect("/");
+} catch (error) {
+        console.error(error);
         res.status(500).send("Errore interno del server");
     }
 });
@@ -107,7 +99,7 @@ app.use(express.static('pub'));
 
 //  route per la Home Page
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/pub/index.html');
+  res.sendFile(__dirname + "index.html");
 });
 
 
@@ -119,7 +111,6 @@ app.listen(port, () => {
     console.log(`Server in esecuzione sulla porta ${port}`);
 });
 
-
 //schema db login
 
 
@@ -127,17 +118,17 @@ app.listen(port, () => {
 app.get("/register", function (req, res) {
     res.render("home");
 });
- 
+
 //  user signup
 app.post("/register", async (req, res) => {
     const user = await User.create({
       username: req.body.username,
       password: req.body.password
     });
-   
+
     return res.render("mod");
   });
- 
+
 
 // Mostra il form di login
 app.get("/login", (req, res) => {
@@ -148,7 +139,7 @@ app.get("/login", (req, res) => {
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
- 
+
 // user login
 app.post("/login", async function(req, res) {
     try {
@@ -171,7 +162,7 @@ app.post("/login", async function(req, res) {
     }
 });
 
- 
+
 // user logout 
 app.get("/logout", function (req, res) {
     req.logout(function(err) {
@@ -179,9 +170,9 @@ app.get("/logout", function (req, res) {
         res.redirect('/index');
       });
 });
- 
- 
- 
+
+
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
     res.redirect("/login");
@@ -224,7 +215,7 @@ app.get("/prenotazione", async (req, res) => {
 // Gestione della richiesta di prenotazione
 app.post("/prenotazione", async (req, res) => {
     try {
-        
+
 
         if (!req.body.nome || !req.body.cognome || !req.body.tipoVisita || !req.body.data) {
             return res.status(400).send("Compila tutti i campi del modulo di prenotazione.");
@@ -245,7 +236,7 @@ app.post("/prenotazione", async (req, res) => {
         // Salva la prenotazione nel database
         await newPrenotazione.save();
 
-       
+
 
         res.render("home"); // Redirect alla pagina di prenotazione
     } catch (error) {
@@ -291,11 +282,11 @@ module.exports = Prenotazione;
 
 const ImageSchema = new mongoose.Schema({
     path: { type: String, required: true },
-  
+
   });
-  
+
   const ImageModel = mongoose.model('Image', ImageSchema);
-  
+
   // Salva il percorso dell'immagine nel database
 
   const nuovaImmagine = new ImageModel({ path: '"C:\Users\vince\OneDrive\Desktop\db.png"' });
@@ -307,7 +298,7 @@ const ImageSchema = new mongoose.Schema({
       console.error('Errore nel salvataggio dell\'immagine nel database:', errore);
     });
 
-    
+
     const Image = require('./index.js');
 
 
@@ -319,11 +310,11 @@ const ImageSchema = new mongoose.Schema({
     app.get('/uploads/:id', async (req, res) => {
         try {
             const image = await ImageModel.findById(req.params.id.replace(/[{()}]/g, ''));
-    
+
             if (!image) {
                 return res.status(404).send('Immagine non trovata');
             }
-    
+
             // Invia i dati dell'immagine come risposta
             res.contentType(image.contentType);
             res.send(image.data);
@@ -335,7 +326,7 @@ const ImageSchema = new mongoose.Schema({
 
 
 
-    
+
 
 
 
@@ -345,7 +336,7 @@ const domandeSchema = new mongoose.Schema({
     nome: String,
     domanda1: String,
     domanda2: String,
-    
+
 });
 
 const DomandeModel = mongoose.model('Domande', domandeSchema);
@@ -523,30 +514,28 @@ app.get('/corsi', async (req, res) => {
       res.status(500).send('Errore interno del server');
     }
   });
-  
-  // Endpoint per l'aggiunta di un nuovo corso
-  app.post('/aggiungi-corso', async (req, res) => {
-    try {
-      // Estrai i dati dal body della richiesta
-      const { titolo, descrizione, istruttore, videoURL } = req.body;
-  
-      // Crea un nuovo corso utilizzando il modello
-      const nuovoCorso = new Corso({
-        titolo,
-        descrizione,
-        istruttore,
-        videoURL
-      });
-  
-      // Salva il nuovo corso nel database
-      await nuovoCorso.save();
-  
-      res.redirect('/mod');
-    } catch (error) {
-      console.error('Errore durante l\'aggiunta del corso:', error);
-      res.status(500).send('Errore interno del server');
-    }
-  });
+app.post('/aggiungi-corso', async (req, res) => {
+  try {
+    // Estrai i dati dal body della richiesta
+    const { titolo, descrizione, istruttore, videoURL } = req.body;
+
+    // Crea un nuovo corso utilizzando il modello
+    const nuovoCorso = new Corso({
+      titolo,
+      descrizione,
+      istruttore,
+      videoURL
+    });
+
+    // Salva il nuovo corso nel database
+    await nuovoCorso.save();
+
+    res.redirect('/mod');
+  } catch (error) {
+    console.error('Errore durante l\'aggiunta del corso:', error);
+    res.status(500).send('Errore interno del server');
+  }
+});
 
 
 
@@ -569,85 +558,33 @@ app.get('/corsi', async (req, res) => {
 
 
 
+// Definizione dello schema del modello (Nome)
+const nomeSchema = new mongoose.Schema({
+  nome: String
+});
 
+// Creazione del modello Nome
+const Nome = mongoose.model('Nome', nomeSchema);
 
+// Middleware per analizzare il corpo della richiesta POST
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Endpoint per gestire la richiesta POST di salvataggio del nome
+app.post('/salva-nome', async (req, res) => {
+  try {
+    const { nome } = req.body;
 
+    // Creazione di un nuovo documento Nome
+    const nuovoNome = new Nome({
+      nome: nome
+    });
 
+    // Salvataggio nel database
+    await nuovoNome.save();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//// vecchi codici di test e prova
-
-
-
-//  vecchi collegamenti pagine
-//app.get('/prenotazione', (req, res) => {
-  //res.sendFile(__dirname + '/pub/prenotazione.html');
-//});
-//app.get('/login', (req, res) => {
-    //res.sendFile(__dirname + '/pub/login.html');
-  //});
-//pdf visita medica
-//function generatePDF(data) {
-   // const pdf = new jsPDF();
-    // Aggiungi il contenuto al PDF
-    //pdf.text(20, 20, 'Dati Paziente:');
-    //pdf.text(20, 30, 'Nome: ' + data.nome);
-   // pdf.text(20, 40, 'Cognome: ' + data.cognome);
-   //const pdfPath = __dirname + "/pdfs/prenotazione_" + data._id + ".pdf";
-
-    // Salva il PDF
-    //pdf.save(pdfPath, () => {
-   //     console.log("PDF salvato:", pdfPath);
-   //// });
-//}
-
-
-//const Grid = require('gridfs-stream');
-//Grid.mongo = mongoose.mongo;
-//const gfs = Grid(mongoose.connection.db);
-//
-//app.get('/video/:filename', (req, res) => {
-    //const readstream = gfs.createReadStream({ filename: req.params.filename });
-    //readstream.pipe(res);
-////});
-
-//app.get('/video/:filename', (req, res) => {
-    ///const readstream = gfs.createReadStream({ filename: req.params.filename });
-    //readstream.pipe(res);
-//});
-
-//app.listen(port, () => {
-   // console.log(`Server in esecuzione sulla porta ${port}`);
-//});
-
-
-
-// Codice lato client
-// Codice lato client
-//const imgElement = document.getElementById('immagineVisibile');
-//imgElement.src = `/uploads/${idDellImmagineDaVisualizzare}`;
-
-
-// index.js
-////const imgElement = document.getElementById('immagineVisibile');
-
-// Immagine da visualizzare (sostituisci con il percorso appropriato o URL dell'immagine)
-//const imagePath = '/uploads/65c676eb1c54e06c8cdfa042';
-
-// Imposta il percorso dell'immagine come sorgente dell'elemento immagine
-//imgElement.src = imagePath;
-
-
+    res.send('Nome salvato con successo!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Errore interno del server durante il salvataggio del nome.');
+  }
+});
